137dad11a783feaa372c76c2cf1874d440182914
bakiasib <90704194+bakiasib@users.noreply.github.com>
2022-12-14 15:52:57 +0100

    Added Node functionalities

--- part-2/ASTARBusQ.py
+++ part-2/ASTARBusQ.py
@@ -7,7 +7,7 @@ class Student(object):
     '''
     Student definition
     '''
-    def __init__(self, id, year, troublesome, red_mobility, id_sibling):
+    def __init__(self, id, year, troublesome, red_mobility, id_sibling, seat):
         self.id = id
 
         if year not in (1, 2): raise ValueError
@@ -27,6 +27,8 @@ class Student(object):
 
         self.id_sibling = id_sibling
 
+        self.seat = seat
+
 
     def __str__(self):
         string = str(self.id)
@@ -41,27 +43,67 @@ class Student(object):
 
 
 class Node():
-    state = []
-    cost = 0
+
+    def __init__(self, current_state: list = [], current_cost: int = 0, current_state_costs =[]):
+        self.state = current_state
+        self.cost = current_cost
+        self.student_costs = current_state_costs
 
     def __str__(self) -> str:
-        return str(self.state)
+        return str(self.state) 
 
     def __eq__(self, other: object) -> bool:
         return self.state == other.state
 
-    def calculateCost():
-        pass
+    def __lt__(self, other: object) -> bool:
+        return self.cost < other.cost
 
-    def isGoal(data: dict):
-        pass
+    def calculateCost(self):
+        new_cost = self.student_costs
+        for i in range(len(new_cost)):
+            # A student that is in the queue right behind a student with reduced mobility must help them get on the bus. Because of this, the time taken by the next student is the time of the reduced mobility student
+            if self.state[i].red_mobility:
+                new_cost[i+1] = new_cost[i]
+                new_cost[i] = 0
+                # if a troublesome student helps a student with reduced mobility enter the bus, the time taken by both of them would be double
+                if(self.state[i+1].troublesome):
+                    new_cost[i+1] *= 2
+            if new_cost[i].troublesome:
+                # A troublesome student will double the time the students in front and behind them take to enter the bus.
+                new_cost[i+1] *= 2
+                new_cost[i-1] *= 2
+                #A troublesome student will double the needed to enter the bus for ALL students that are behind him in the queue and have an assigned seat that is higher than his
+                for j in range(i+1, len(new_cost)):
+                    if self.state[j].seat > self.state[i].seat:
+                        new_cost[j] *= 2
+        total_cost = 0
+        for cost in new_cost:
+            #the total cost of the queue is the sum of the costs of each student
+            total_cost += cost
+        return total_cost
 
-    def descendants(data: dict) -> tuple:
-        """
-        Returns a tuple of nodes, with their cost calculated
-        """
-        pass
+    def isGoal(self, data: dict):
+        # the goal is reached when the queue is full and the last student in the queue is not a student with reduced mobility
+        return len(self.state) == len(data) and not self.state[-1].red_mobility
 
+    def descendants(self, data: tuple) -> tuple:
+        descendant = []
+        for student in data:
+            # a student can be added to the queue if he is not already in the queue
+            if student in self.state: continue
+            # a student with reduced mobility can only be added to the queue if the last student in the queue is not a student with reduced mobility
+            if self.state[-1].red_mobility and student.red_mobility: continue
+            # the cost of a regual student is 1 and a reduced mobility student is 3
+            student_cost = 1
+            if student.red_mobility: student_cost = 3
+            # Create a new node with the new state, cost and state_costs and add it to the list of descendants
+            descendant.append(Node(self.state + [student], self.calculateCost(), self.student_costs + [student_cost]))
+        
+        return tuple(descendant)
+
+def test_node_descendants():
+    first_node = Node()
+    data = (Student(1, 1, "C", "X", 0, 1), Student(2, 1, "X", "X", 0, 2), Student(3, 1, "X", "R", 0, 3))
 
 
 def h1(node: Node) -> int:
